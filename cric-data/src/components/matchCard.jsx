8663da6290caf0fbc06159e91af02697a45dfe36
Yashwanth-Racherla <ignitingyash@gmail.com>
2022-05-18 19:29:09 +0530

    match details popup responsive CSS finished

--- cric-data/src/components/matchCard.jsx
+++ cric-data/src/components/matchCard.jsx
@@ -8,8 +8,8 @@ const MatchCard = () => {
   const [cricMatchDetails, setCricMatchDetails] = useState(null);
   const [showMatchDetailsPopup, setShowMatchDetailsPopup] = useState(false);
 
-  const apiKey = "2f0d633d-aed1-474b-9fa4-8bb1af008ca9";
-  // const apiKey = "8474bb0f-cb30-48bc-8272-1cc7a31e3dee";
+  // const apiKey = "2f0d633d-aed1-474b-9fa4-8bb1af008ca9";
+  const apiKey = "8474bb0f-cb30-48bc-8272-1cc7a31e3dee";
   // const apiKey = "af3ef40f-1364-4e71-9ae1-dc153e43f49d";
   // const apiKey = "ce2ea15b-deaf-491b-a809-7367ab6d9024";
   // const apiKey = "6e9c3ee5-acbb-4168-906b-dda3fb5b4acd"
@@ -66,30 +66,39 @@ const MatchCard = () => {
       {showMatchDetailsPopup && (
         <div className="show-match-details-popup">
           <div className="show-match-details">
-            <h2 className="text-center">{cricData.data.info.name}</h2>
-            <div className="py-10">
-              <p>{`Match : ${cricMatchDetails.name}`}</p>
-              <p>{`Date : ${new Date(
-                cricMatchDetails.dateTimeGMT
-              ).toDateString()}`}</p>
-              <p>{`Venue : ${cricMatchDetails.venue}`}</p>
-              <p>{`Toss : ${cricMatchDetails.tossWinner} won the toss and opted to ${cricMatchDetails.tossChoice} first`}</p>
+            <h2 className="text-center text-2xl font-bold pt-6">
+              {cricData.data.info.name}
+            </h2>
+            <div className=" py-10 font-semibold">
+              <div className="flex">
+                <p className="shrink-0">{`Match : `}</p>
+                <p>{cricMatchDetails.name}</p>
+              </div>
+              <div className="flex">
+                <p className="shrink-0">{`Date : `}</p>
+                <p>{new Date(cricMatchDetails.dateTimeGMT).toDateString()}</p>
+              </div>
+              <div className="flex">
+                <p className="shrink-0">{`Venue : `}</p>
+                <p>{cricMatchDetails.venue}</p>
+              </div>
+              <div className="flex ">
+                <p className="shrink-0">{`Toss : `}</p>
+                <p>{`${cricMatchDetails.tossWinner} won the toss and opted to ${cricMatchDetails.tossChoice} first`}</p>
+              </div>
             </div>
-            {/* <div className="flex">
-              <p>Match:</p>
-              <p>{cricMatchDetails.name}</p>
-            </div> */}
+
             <div className="flex justify-center">
-              <div className="btn">
+              <div className="btn mx-2">
                 <p>
-                  {cricMatchDetails.score[0]?.inning?.replace("Inning 1", "")}
+                  {cricMatchDetails?.score[0]?.inning?.replace("Inning 1", "")}
                 </p>
                 <p>
-                  {`${cricMatchDetails.score[0]?.r}/${cricMatchDetails.score[0]?.w}
+                  {`${cricMatchDetails?.score[0]?.r}/${cricMatchDetails.score[0]?.w}
                   (${cricMatchDetails.score[0]?.o})`}
                 </p>
               </div>
-              <div className="btn">
+              <div className="btn mx-2">
                 <p>
                   {cricMatchDetails.score[1]?.inning?.replace("Inning 1", "")}
                 </p>
@@ -99,7 +108,9 @@ const MatchCard = () => {
                 </p>
               </div>
             </div>
-            <div className="text-center">{cricMatchDetails.status}</div>
+            <div className="text-center text-[#FFC371] font-bold sm:text-xl p-4">
+              {cricMatchDetails.status}
+            </div>
             <div className="close-match-details">
               <FontAwesomeIcon
                 icon={faXmark}
@@ -110,9 +121,55 @@ const MatchCard = () => {
         </div>
       )}
 
+      {cricData?.data?.matchList?.map((cricMatch) => {
+        const matchDate = new Date(cricMatch.dateTimeGMT);
+        const matchTime = matchDate.toDateString();
+
+        return (
+          <div key={cricMatch?.id} className="match-data">
+            {cricMatch?.teamInfo?.length > 1 ? (
+              <div className="flex justify-center h-2/3 ">
+                <div className="team-vs-team w-2/5">
+                  <img
+                    className="w-20"
+                    src={cricMatch?.teamInfo[0]?.img}
+                    alt="Team 1 logo"
+                  />
+                  <p> {cricMatch?.teamInfo[0]?.name} </p>
+                </div>
+                <div className="team-vs-team w-1/5">
+                  <img className="w-16" src="./vs image.jpg" alt="vs logo" />
+                </div>
+                <div className="team-vs-team w-2/5">
+                  <img
+                    className="w-20"
+                    src={cricMatch?.teamInfo[1]?.img}
+                    alt="Team 1 logo"
+                  />
+                  <p> {cricMatch?.teamInfo[1]?.name} </p>
+                </div>
+              </div>
+            ) : (
+              "Team 1 vs Team 2"
+            )}
+
+            <div>
+              <p>{matchTime}</p>
+
+              <button
+                className="btn"
+                onClick={() => showMatchDetails(cricMatch.id)}
+              >
+                Show Match Details
+              </button>
+            </div>
+          </div>
+        );
+      })}
+
       {cricData?.data?.matchList
         ?.filter((cricMatch) => {
-          return cricMatch.status !== "Match not started";
+          return cricMatch.status === "Match not started";
         })
         .map((cricMatch) => {
           const matchDate = new Date(cricMatch.dateTimeGMT);
