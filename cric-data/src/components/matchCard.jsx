ae2f7e53df4d5d37c7502b6cc1bfb4aff3125e0a
Yashwanth-Racherla <ignitingyash@gmail.com>
2022-05-23 20:23:45 +0530

    multiple series api call js updated

--- cric-data/src/components/matchCard.jsx
+++ cric-data/src/components/matchCard.jsx
@@ -3,29 +3,33 @@ import React from "react";
 const MatchCard = ({ cricMatch, showMatchData }) => {
   return (
     <>
-      <div key={cricMatch?.id} className="match-data">
+      <div className="match-data">
         {cricMatch?.teamInfo?.length > 1 ? (
-          <div className="flex justify-center h-2/3 ">
-            <div className="team-vs-team w-2/5">
-              <img
-                className="w-20"
-                src={cricMatch?.teamInfo[0]?.img}
-                alt="Team 1 logo"
-              />
-              <p> {cricMatch?.teamInfo[0]?.name} </p>
+          <>
+            <p className="pb-2 h-14">{cricMatch?.name}</p>
+
+            <div className="flex justify-center h-[152px]">
+              <div className="team-vs-team w-2/5">
+                <img
+                  className="w-20"
+                  src={cricMatch?.teamInfo[0]?.img}
+                  alt="Team 1 logo"
+                />
+                <p> {cricMatch?.teamInfo[0]?.name} </p>
+              </div>
+              <div className="team-vs-team w-1/5">
+                <img className="w-16" src="./vs image.jpg" alt="vs logo" />
+              </div>
+              <div className="team-vs-team w-2/5">
+                <img
+                  className="w-20"
+                  src={cricMatch?.teamInfo[1]?.img}
+                  alt="Team 1 logo"
+                />
+                <p> {cricMatch?.teamInfo[1]?.name} </p>
+              </div>
             </div>
-            <div className="team-vs-team w-1/5">
-              <img className="w-16" src="./vs image.jpg" alt="vs logo" />
-            </div>
-            <div className="team-vs-team w-2/5">
-              <img
-                className="w-20"
-                src={cricMatch?.teamInfo[1]?.img}
-                alt="Team 1 logo"
-              />
-              <p> {cricMatch?.teamInfo[1]?.name} </p>
-            </div>
-          </div>
+          </>
         ) : (
           "Team 1 vs Team 2"
         )}
@@ -36,6 +40,7 @@ const MatchCard = ({ cricMatch, showMatchData }) => {
           <button className="btn" onClick={() => showMatchData(cricMatch.id)}>
             Show Match Details
           </button>
+          <p>{cricMatch?.status}</p>
         </div>
       </div>
     </>
--- cric-data/src/components/matchCards.jsx
+++ cric-data/src/components/matchCards.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
@@ -13,25 +13,10 @@ const MatchCards = () => {
   const [inningsNumber, setInningsNumber] = useState(0);
 
   // const apiKey = "2f0d633d-aed1-474b-9fa4-8bb1af008ca9";
-  const apiKey = "8474bb0f-cb30-48bc-8272-1cc7a31e3dee";
+  // const apiKey = "8474bb0f-cb30-48bc-8272-1cc7a31e3dee";
   // const apiKey = "af3ef40f-1364-4e71-9ae1-dc153e43f49d";
   // const apiKey = "ce2ea15b-deaf-491b-a809-7367ab6d9024";
-  // const apiKey = "6e9c3ee5-acbb-4168-906b-dda3fb5b4acd"
-
-  const seriesList = [
-    // {
-    //   id: "47b54677-34de-4378-9019-154e82b9cc1a",
-    //   name: "IPL",
-    // },
-    // {
-    //   id: "17976dfb-1371-488c-b420-c7d2e2cd7d14",
-    //   name: "UgandaVsNepal",
-    // },
-    {
-      id: "b5108aba-0694-42c4-b9eb-8f5eda5a41c7",
-      name: "BangladeshVsSrilanka",
-    },
-  ];
+  const apiKey = "6e9c3ee5-acbb-4168-906b-dda3fb5b4acd";
 
   const byDate = (a, b) => {
     if (new Date(a.dateTimeGMT).valueOf() > new Date(b.dateTimeGMT).valueOf()) {
@@ -44,27 +29,45 @@ const MatchCards = () => {
       return 0;
     }
   };
+  //  =================================
+  //  Multiple Series Fetching Api
+  //  =================================
+  const seriesList = [
+    {
+      id: "47b54677-34de-4378-9019-154e82b9cc1a",
+      name: "IPL",
+    },
+    {
+      id: "b5108aba-0694-42c4-b9eb-8f5eda5a41c7",
+      name: "BangladeshVsSrilanka",
+    },
+    {
+      id: "17976dfb-1371-488c-b420-c7d2e2cd7d14",
+      name: "UgandaVsNepal",
+    },
+  ];
+  const getSeriesData = async () => {
+    const tempSeriesArr = [];
 
-  const tempSeriesArr = [];
-  console.log(seriesList.length);
-  seriesList.forEach((series) => {
-    const getSeriesData = async () => {
+    for (const series of seriesList) {
       const seriesApiData = await fetch(
         `https://api.cricapi.com/v1/series_info?apikey=${apiKey}&offset=0&id=${series.id}`
       );
 
       await seriesApiData.json().then((res) => {
-        // res.data?.matchList?.sort(byDate);
+        res.data?.matchList?.sort(byDate);
         tempSeriesArr.push(res);
-        // setCricSeriesData((prevState) => [...prevState, res]);
         setCricSeriesData(tempSeriesArr);
       });
-    };
+    }
+  };
+
+  useEffect(() => {
     getSeriesData();
-  });
+  }, []);
 
   //  ====================================================================
-  //  showMatchData() function called on jsx button element
+  //  showMatchData() function called on jsx button element ( Match Details )
   //  ====================================================================
 
   const showMatchData = async (id) => {
@@ -80,20 +83,21 @@ const MatchCards = () => {
     await matchScoreCardApiData.json().then((res) => {
       setCricMatchScoreCard(res.data);
       setShowMatchDataPopup(true);
-      console.log(res.data);
     });
   };
 
   //  ====================================================================
-  //  closeMatchData() function called on jsx button element
+  //  closeMatchData() function called on jsx button element( X Button)
   //  ====================================================================
   const closeMatchData = () => {
     setShowMatchDataPopup(false);
     setInningsNumber(0);
   };
-
-  const moreMatches = 3;
-  const [matchesToShow, setMatchesToShow] = useState(6);
+  //  ====================================================================
+  //  showMoreMatches function called on jsx button element (More Matches)
+  //  ====================================================================
+  const moreMatches = 1;
+  const [matchesToShow, setMatchesToShow] = useState(3);
   const showMoreMatches = () => {
     setMatchesToShow(matchesToShow + moreMatches);
   };
@@ -101,38 +105,41 @@ const MatchCards = () => {
   return (
     <>
       {cricSeriesData.map((series) => {
-        <>
-          <div className="p-2 font-extrabold text-xl sm:text-3xl">
-            <p>{series?.data?.info?.name}</p>
-          </div>
-          <div className="md:flex md:justify-center md:flex-wrap">
-            {series?.data?.matchList
-              ?.filter((cricMatch) => {
-                return cricMatch.status !== "Match not started";
-              })
-              .slice(0, matchesToShow)
-              .map((cricMatch) => {
-                return (
-                  <MatchCard
-                    cricMatch={cricMatch}
-                    showMatchData={showMatchData}
-                  />
-                );
-              })}
-          </div>
+        return (
+          <React.Fragment key={series.id}>
+            <div className="p-2 font-extrabold text-xl sm:text-3xl">
+              <p>{series?.data?.info?.name}</p>
+            </div>
+            <div className="md:flex md:justify-center md:flex-wrap">
+              {series?.data?.matchList
+                ?.filter((cricMatch) => {
+                  return cricMatch.status !== "Match not started";
+                })
+                .slice(0, matchesToShow)
+                .map((cricMatch) => {
+                  return (
+                    <MatchCard
+                      key={cricMatch?.id}
+                      cricMatch={cricMatch}
+                      showMatchData={showMatchData}
+                    />
+                  );
+                })}
+            </div>
 
-          {matchesToShow <
-            series?.data?.matchList.filter((cricMatch) => {
-              return cricMatch.status !== "Match not started";
-            }).length && (
-            <button
-              className="btn p-2 m-2 text-blue-600 font-bold text-xl"
-              onClick={() => showMoreMatches()}
-            >
-              More matches
-            </button>
-          )}
-        </>;
+            {matchesToShow <
+              series?.data?.matchList.filter((cricMatch) => {
+                return cricMatch.status !== "Match not started";
+              }).length && (
+              <button
+                className="btn p-2 m-2 text-blue-600 font-bold text-xl"
+                onClick={() => showMoreMatches()}
+              >
+                More matches
+              </button>
+            )}
+          </React.Fragment>
+        );
       })}
 
       {showMatchDataPopup && (
