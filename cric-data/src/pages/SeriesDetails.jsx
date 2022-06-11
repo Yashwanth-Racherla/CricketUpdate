30fdbf52366daf95676ca591f826df59f284dda9
Rohith Racherla <rohith.racherla@steinias.com>
2022-06-11 15:26:50 +0530

    updated closepopup  with navigate function

--- cric-data/src/pages/SeriesDetails.jsx
+++ cric-data/src/pages/SeriesDetails.jsx
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import MatchCard from "../components/matchCard";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
-import { byDate } from "../helperFunctions/commonFunctions";
+import { byDate, byDateRev } from "../helperFunctions/commonFunctions";
 import { getApiKey } from "../helperData/commonData";
 
 const SeriesDetails = () => {
+  const navigate = useNavigate();
   const { id } = useParams();
   const apiKey = getApiKey();
 
@@ -19,7 +20,7 @@ const SeriesDetails = () => {
     );
 
     await seriesApiData.json().then((res) => {
-      res.data?.matchList?.sort(byDate);
+      // res.data?.matchList?.sort(byDate);
       setSeriesData(res);
     });
   };
@@ -30,33 +31,54 @@ const SeriesDetails = () => {
   }, []);
 
   return (
-    <div className="show-popUp">
-      <div className="show-popUp-data ">
-        <button className="close-popUp">
-          <Link to="/">
-            <FontAwesomeIcon icon={faXmark} />
-          </Link>
+    <div className="p-2 text-sm">
+      <button className="close-popUp">
+        <button onClick={() => navigate(-1)}>
+          <FontAwesomeIcon icon={faXmark} />
         </button>
-        <div className="p-2 font-extrabold text-xl sm:text-3xl mt-6">
-          <p className="text-center">{seriesData?.data?.info?.name}</p>
-        </div>
+      </button>
+      <div className="p-2 font-extrabold text-xl sm:text-3xl mt-6">
+        <p className="text-center">{seriesData?.data?.info?.name}</p>
+      </div>
 
-        <div className="flex flex-wrap">
-          {seriesData?.data?.matchList
-            ?.filter((cricMatch) => {
-              return cricMatch.status !== "Match not started";
-            })
-            .map((cricMatch) => {
-              return (
-                <MatchCard
-                  key={cricMatch?.id}
-                  cricMatch={cricMatch}
-                  className="h-auto w-full sm:w-[48%] lg:w-[30%] mx-auto my-2"
-                  // showMatchData={showMatchData}
-                />
-              );
-            })}
-        </div>
+      <h2 className="font-bold text-3xl">Results</h2>
+
+      <div className="flex flex-wrap">
+        {seriesData?.data?.matchList
+          ?.sort(byDate)
+          .filter((cricMatch) => {
+            return cricMatch.status !== "Match not started";
+          })
+          .map((cricMatch) => {
+            return (
+              <MatchCard
+                key={cricMatch?.id}
+                cricMatch={cricMatch}
+                className="h-auto w-full sm:w-[48%] lg:w-[30%] mx-auto my-2"
+                // showMatchData={showMatchData}
+              />
+            );
+          })}
+      </div>
+
+      {<h2 className="font-bold text-3xl">Upcoming Matches</h2>}
+
+      <div className="flex flex-wrap">
+        {seriesData?.data?.matchList
+          ?.sort(byDateRev)
+          .filter((cricMatch) => {
+            return cricMatch.status === "Match not started";
+          })
+          .map((cricMatch) => {
+            return (
+              <MatchCard
+                key={cricMatch?.id}
+                cricMatch={cricMatch}
+                className="h-auto w-full sm:w-[48%] lg:w-[30%] mx-auto my-2"
+                // showMatchData={showMatchData}
+              />
+            );
+          })}
       </div>
     </div>
   );
