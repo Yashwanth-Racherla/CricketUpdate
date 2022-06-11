d873c56a89f9a5c4b7488504c6b70311a4a51aeb
Rohith Racherla <rohith.racherla@steinias.com>
2022-06-11 15:27:15 +0530

    minute changes

--- cric-data/src/components/App.jsx
+++ cric-data/src/components/App.jsx
@@ -1,9 +1,9 @@
+import "../css/App.css";
 import NavigationBar from "./navigationBar";
+import Home from "../pages/Home";
 import MatchDetails from "../pages/MatchDetails";
 import SeriesDetails from "../pages/SeriesDetails";
-import "../css/App.css";
 import { Routes, Route } from "react-router-dom";
-import Home from "../pages/Home";
 
 const App = () => {
   return (
@@ -12,7 +12,6 @@ const App = () => {
 
       <Routes>
         <Route path="/" element={<Home />} />
-
         <Route path="/seriesdetails/:id" element={<SeriesDetails />} />
         <Route path="/matchdetails/:id" element={<MatchDetails />} />
       </Routes>
