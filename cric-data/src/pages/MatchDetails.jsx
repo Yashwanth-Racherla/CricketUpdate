16ae650a848eeda2212fbb79dd4f4fc5a09b39f9
Rohith Racherla <rohith.racherla@steinias.com>
2022-06-11 15:26:18 +0530

    updated closePopUp image with navigate function

--- cric-data/src/pages/MatchDetails.jsx
+++ cric-data/src/pages/MatchDetails.jsx
@@ -2,27 +2,30 @@ import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import MatchScorecard from "../components/matchScorecard";
-import { Link, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { getApiKey } from "../helperData/commonData";
 
 const MatchDetails = () => {
   const { id } = useParams();
   const apiKey = getApiKey();
-  const [matchData, setMatchData] = useState(null);
-  const [scoreCard, setScoreCard] = useState(null);
+  const navigate = useNavigate();
+  const [matchInfo, setMatchInfo] = useState(null);
+  const [matchScoreCard, setMatchScoreCard] = useState(null);
+  const [inningsNum, setInningsNum] = useState(0);
 
   const showMatchData = async () => {
-    const getMatchData = await fetch(
+    const getMatchInfo = await fetch(
       `https://api.cricapi.com/v1/match_info?apikey=${apiKey}&offset=0&id=${id}`
     );
-    const getScoreCard = await fetch(
+    const getMatchScoreCard = await fetch(
       `https://api.cricapi.com/v1/match_scorecard?apikey=${apiKey}&offset=0&id=${id}`
     );
-    await getMatchData.json().then((res) => {
-      setMatchData(res.data);
+    await getMatchInfo.json().then((res) => {
+      setMatchInfo(res.data);
     });
-    await getScoreCard.json().then((res) => {
-      setScoreCard(res.data);
+    await getMatchScoreCard.json().then((res) => {
+      setMatchScoreCard(res.data);
+      // console.log(res);
     });
   };
   useEffect(() => {
@@ -30,64 +33,63 @@ const MatchDetails = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return (
-    <div className="show-popUp">
-      <div className="show-popUp-data">
-        {/* ======= close the pop up button ====== */}
-        <button className="close-popUp">
-          <Link to="/">
-            <FontAwesomeIcon icon={faXmark} />
-          </Link>
-        </button>
-        {/* ======= Match Status ======  */}
-        <h2 className="font-bold py-4 text-lg sm:text-xl">
-          {matchData?.status}
-        </h2>
-        {/* ===== Team Names ====== */}
-        <div className="flex flex-wrap">
-          {scoreCard?.scorecard?.map((teamScoreCard, index) => {
-            return (
-              <button
-                key={teamScoreCard.inning}
-                className="w-1/2 btn flex flex-col items-center justify-between font-bold sm:text-base"
-              >
-                <span>{teamScoreCard.inning.replace("Inning 1", "")}</span>
-                <span>
-                  {`${teamScoreCard.totals?.R}/${teamScoreCard.totals?.W}
-                      (${teamScoreCard.totals?.O})`}
-                </span>
-              </button>
-            );
-          })}
-        </div>
-        {scoreCard?.scorecard?.length > 0 && (
-          <MatchScorecard scoreCardData={scoreCard?.scorecard[0]} />
-        )}
+  const showScoreCard = (id) => {
+    setInningsNum(id);
+  };
 
-        {/* ======= Match Details ====== */}
-        {matchData !== null && (
-          <div className="pt-8 sm:text-lg">
-            <div className="font-semibold">
-              <div className="flex">
-                <p className="shrink-0">{`Match : `}</p>
-                <p>{matchData.name}</p>
-              </div>
-              <div className="flex">
-                <p className="shrink-0">{`Date : `}</p>
-                <p>{new Date(matchData.dateTimeGMT).toDateString()}</p>
-              </div>
-              <div className="flex">
-                <p className="shrink-0">{`Venue : `}</p>
-                <p>{matchData.venue}</p>
-              </div>
-              <div className="flex ">
-                <p className="shrink-0">{`Toss : `}</p>
-                <p>{`${matchData.tossWinner} won the toss and opted to ${matchData.tossChoice} first`}</p>
-              </div>
+  return (
+    <div className="p-2 text-sm">
+      {/* ======= close the pop up button ====== */}
+      <button className="close-popUp" onClick={() => navigate(-1)}>
+        <FontAwesomeIcon icon={faXmark} />
+      </button>
+      {/* ======= Match Status ======  */}
+      <h2 className="font-bold py-4 text-lg sm:text-xl">{matchInfo?.status}</h2>
+      {/* ===== Team Names ====== */}
+      <div className="flex flex-wrap">
+        {matchScoreCard?.scorecard?.map((teamScoreCard, index) => {
+          return (
+            <button
+              key={teamScoreCard.inning}
+              className="w-1/2 btn flex flex-col items-center justify-between font-bold sm:text-base"
+              onClick={() => showScoreCard(index)}
+            >
+              <span>{teamScoreCard.inning.replace("Inning 1", "")}</span>
+              <span>
+                {`${teamScoreCard.totals?.R}/${teamScoreCard.totals?.W}
+                      (${teamScoreCard.totals?.O})`}
+              </span>
+            </button>
+          );
+        })}
+      </div>
+      {matchScoreCard?.scorecard?.length > 0 && (
+        <MatchScorecard scoreCardData={matchScoreCard?.scorecard[inningsNum]} />
+      )}
+
+      {/* ======= Match Details ====== */}
+      {matchInfo !== null && (
+        <div className="pt-8 sm:text-lg">
+          <div className="font-semibold">
+            <div className="flex">
+              <p className="shrink-0">{`Match : `}</p>
+              <p>{matchInfo.name}</p>
+            </div>
+            <div className="flex">
+              <p className="shrink-0">{`Date : `}</p>
+              <p>{new Date(matchInfo.dateTimeGMT).toDateString()}</p>
+            </div>
+            <div className="flex">
+              <p className="shrink-0">{`Venue : `}</p>
+              <p>{matchInfo.venue}</p>
+            </div>
+            <div className="flex ">
+              <p className="shrink-0">{`Toss : `}</p>
+              <p>{`${matchInfo.tossWinner} won the toss and opted to ${matchInfo.tossChoice} first`}</p>
             </div>
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </div>
   );
 };
