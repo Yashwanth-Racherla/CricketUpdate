523b6ee3960dff4378f96fe849c4be1080c7de0e
Yashwanth Racherla <ignitingyash@gmail.com>
2022-07-04 17:39:44 +0530

    Created Sroll to Top component for React Routing to top level

--- cric-data/src/components/App.jsx
+++ cric-data/src/components/App.jsx
@@ -5,17 +5,23 @@ import Home from "../pages/Home";
 import MatchDetails from "../pages/MatchDetails";
 import SeriesDetails from "../pages/SeriesDetails";
 import { Routes, Route } from "react-router-dom";
+import ScrollToTop from "./ScrollToTop";
 
 const App = () => {
   return (
     <div className="bg-[#FFF2F0]">
       <Header />
       <div className="w-[90%] sm:w-4/5 max-w-[1200px] mx-auto ">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/seriesdetails/:seriesId" element={<SeriesDetails />} />
-          <Route path="/matchdetails/:matchId" element={<MatchDetails />} />
-        </Routes>
+        <ScrollToTop>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/seriesdetails/:seriesId"
+              element={<SeriesDetails />}
+            />
+            <Route path="/matchdetails/:matchId" element={<MatchDetails />} />
+          </Routes>
+        </ScrollToTop>
       </div>
       <Footer />
     </div>
