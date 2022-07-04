45254e232b0555a93f3ec74b7bfac800ca1368c5
Yashwanth Racherla <ignitingyash@gmail.com>
2022-07-04 15:54:17 +0530

    Series page code improved
    Mixed results and upcoming matches in one group and run a map function to show all matches

--- cric-data/src/pages/SeriesDetails.jsx
+++ cric-data/src/pages/SeriesDetails.jsx
@@ -34,35 +34,18 @@ const SeriesDetails = () => {
   return isLoading ? (
     <Loading />
   ) : (
-    <div className="text-sm">
+    <div className="text-sm px-2 py-16">
       <button className="close-button " onClick={() => navigate(-1)}>
         <ion-icon class="w-9 h-9" name="close-outline"></ion-icon>
       </button>
-      <div className="px-2 py-16">
-        <h2 className="text-center font-bold text-3xl sm:text-4xl lg:text-5xl ">
-          {seriesData?.data?.info?.name}
-        </h2>
-        <h3 className="font-bold text-3xl py-4">Results</h3>
+      <h2 className="font-bold text-center text-xl sm:text-2xl lg:text-4xl pb-4">
+        {seriesData?.data?.info?.name}
+      </h2>
+      <div>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-          {seriesData?.data?.matchList
-            ?.sort(byDate)
-            .filter((cricMatch) => {
-              return cricMatch.status !== "Match not started";
-            })
-            .map((cricMatch) => {
-              return <MatchCard key={cricMatch?.id} cricMatch={cricMatch} />;
-            })}
-        </div>
-        {<h2 className="font-bold text-3xl py-4">Upcoming Matches</h2>}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-          {seriesData?.data?.matchList
-            ?.sort(byDateRev)
-            .filter((cricMatch) => {
-              return cricMatch.status === "Match not started";
-            })
-            .map((cricMatch) => {
-              return <MatchCard key={cricMatch?.id} cricMatch={cricMatch} />;
-            })}
+          {seriesData?.data?.matchList?.sort(byDateRev).map((cricMatch) => {
+            return <MatchCard key={cricMatch?.id} cricMatch={cricMatch} />;
+          })}
         </div>
       </div>
     </div>
